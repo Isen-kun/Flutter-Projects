f4d7170e0e364e3b0f556afb3b37c04032aabe97
Isen-kun <60184753+Isen-kun@users.noreply.github.com>
2020-12-17 17:21:32 +0530

    initial

---
+++ world_time_app/lib/pages/loading.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void getTime() async {
+    //make the request
+    Response response = await get('http://worldclockapi.com/api/json/utc/now');
+    Map data = jsonDecode(response.body);
+    // print(data);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('loading screen'),
+    );
+  }
+}
